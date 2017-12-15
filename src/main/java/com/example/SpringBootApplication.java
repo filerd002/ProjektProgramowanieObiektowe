0af1378a7f140159dc384fecad50448d79c1eb4b
Filip Erdmann <filerd002@utp.edu.pl>
2017-12-15 18:24:43 +0100

    Aplikacja stabilna, z wdrożonym spring boot seciurity

---
+++ src/main/java/com/example/SpringBootApplication.java
@@ -0,0 +1,7 @@
+package com.example;
+
+/**
+ * Created by Panda on 2017-06-11.
+ */
+public @interface SpringBootApplication {
+}
---
+++ src/main/java/com/example/WebSecurityConfig.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example;
+
+/**
+ *
+ * @author erdi5
+ */
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+            .logout()
+                .permitAll();
+             http.csrf().disable();
+    }
+
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+                .withUser("Filip").password("perszing").roles("ADMIN");
+        auth
+            .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
+
+    }}
