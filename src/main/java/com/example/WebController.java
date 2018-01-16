0a3e551ed6e0a3cbf64e870ca1467dea18a23579
Filip Erdmann <filerd002@utp.edu.pl>
2018-01-16 19:04:46 +0100

    -Spring boot seciurity dzialajacy z baza danych -front-end dla logowania oraz szkielet dla panelu administratora oraz uzytkownika - skrypt tworzenia bazy danych ---aplikacja podpieta pod lokalna baze danych, gdzie docelowo zostanie podpieta pod baze znalna---

---
+++ src/main/java/com/example/WebController.java
@@ -0,0 +1,85 @@
+package com.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+   
+    @RequestMapping(value={"/","home"})
+        public String home(){
+            return "home";
+        }
+   
+  
+    @RequestMapping(value="/admin")
+    public String admin(){
+        return "admin";
+    }
+   
+     @RequestMapping(value="/admin_zarz_user")
+    public String admin_zarz_user(){
+        return "admin_zarz_user";
+    }
+         @RequestMapping(value="/admin_zarz_dzialka")
+    public String admin_zarz_dzialka(){
+        return "admin_zarz_dzialka";
+    }
+         @RequestMapping(value="/admin_liczniki")
+    public String admin_liczniki(){
+        return "admin_liczniki";
+    }
+         @RequestMapping(value="/admin_upload")
+    public String admin_upload(){
+        return "admin_upload";
+    }
+         @RequestMapping(value="/admin_wyciagiJS")
+    public String admin_wyciagiJS(){
+        return "admin_wyciagiJS";
+    }
+         @RequestMapping(value="/admin_raport")
+    public String admin_raport(){
+        return "admin_raport";
+    }
+        
+    
+     @RequestMapping(value="/user")
+    public String user(){
+        return "user";
+    }
+
+      @RequestMapping(value="/user_Rozrachunki")
+    public String user_Rozrachunki(){
+        return "user_Rozrachunki";
+    }
+       @RequestMapping(value="/user_Zobowiazania")
+    public String user_Zobowiazania(){
+        return "user_Zobowiazania";
+    }
+     
+       @RequestMapping(value="/user_bank")
+    public String user_bank(){
+        return "user_bank";
+    }
+       @RequestMapping(value="/user_licznik")
+    public String user_licznik(){
+        return "user_licznik";
+    }
+    
+          @RequestMapping(value="/user_ustawienia")
+    public String user_ustawienia(){
+        return "user_ustawienia";
+    }
+  
+    @RequestMapping(value={"/login"})
+    public String login(){
+        return "login";
+    }
+   
+
+   
+    @RequestMapping(value="/403")
+    public String Error403(){
+        return "403";
+    }
+}
