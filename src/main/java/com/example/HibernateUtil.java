aecaf11240a3e3c649b3c1bd6e55442210346440
filerd002 <filerd002@utp.edu.pl>
2018-02-14 02:06:45 +0100

    Wersja finalna dla Heroku

--- src/main/java/com/example/HibernateUtil.java
+++ src/main/java/com/example/HibernateUtil.java
@@ -9,27 +9,41 @@ package com.example;
  *
  * @author erdi5
  */
-
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
- 
+
+
+
 public class HibernateUtil {
-    private static SessionFactory sessionFactory;
-     
-    public static SessionFactory getSessionFactory() {
-        if (sessionFactory == null) {
-            // loads configuration and mappings
-            Configuration configuration = new Configuration().configure();
-            ServiceRegistry serviceRegistry
-                = new StandardServiceRegistryBuilder()
-                    .applySettings(configuration.getProperties()).build();
-             
-            // builds a session factory from the service registry
-            sessionFactory = configuration.buildSessionFactory(serviceRegistry);            
-        }
-         
-        return sessionFactory;
+
+private static final SessionFactory sessionFactory;
+
+
+
+static {
+
+    try {
+
+        sessionFactory = new Configuration().configure().buildSessionFactory();
+
+    } catch (Throwable ex) {
+
+        System.err.println("Initial session factory creation failed" + ex);
+
+        throw new ExceptionInInitializerError(ex);//HibernateUtil.java:14
+
     }
+
 }
+
+
+
+public static SessionFactory getSessionFactory() {
+
+    return sessionFactory;
+
+    }
+
+}
+
